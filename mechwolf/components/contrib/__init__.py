d71e42ca792db20719b5129a5b46b5affb8d2547
Prashant Kumar <pprashan@iu.edu>
2025-03-17 19:12:03 -0400

    _

--- mechwolf/components/contrib/__init__.py
+++ mechwolf/components/contrib/__init__.py
@@ -7,15 +7,3 @@ from .labjack import LabJack
 from .varian import VarianPump
 from .vici import ViciValve
 from .vicipump import ViciPump
-from .tmc_stepper import TMCPump  # Add this line
-
-__all__ = [
-    "ArduinoSensor",
-    "GilsonFC203", 
-    "GsiocInterface",
-    "LabJack",
-    "VarianPump",
-    "ViciValve", 
-    "ViciPump",
-    "TMCPump"  # Add this line
-]
