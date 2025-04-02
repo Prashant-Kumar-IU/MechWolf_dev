d55c1b7260462a29f4f6b016386a1ad647b6c31b
Prashant Kumar <pprashan@iu.edu>
2025-04-02 17:40:42 -0400

    changed file name

--- mechwolf/DataEntry/ProtocolDev/ProtocolNSyringes.py
+++ mechwolf/DataEntry/ProtocolDev/ProtocolNSyringes.py
@@ -26,7 +26,7 @@ Methods:
 """
 from mechwolf.core.protocol import Protocol
 from mechwolf.components import ActiveComponent
-from .ProtocolNSyringesGUICreator import ProtocolGUI, PumpConfig
+from .ProtocolNSyringesUICreator import ProtocolGUI, PumpConfig
 import re
 from typing import List, Dict, Any, Union, Tuple, Optional
 
