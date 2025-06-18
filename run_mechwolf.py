5a03e5679b7197be82fcb125a9fcf5a23799eaad
Prashant Kumar <pprashan@iu.edu>
2025-06-18 04:08:33 -0400

    Add entry point script for PyInstaller

---
+++ run_mechwolf.py
@@ -0,0 +1,8 @@
+import mechwolf
+
+if __name__ == '__main__':
+    # You should replace this with the actual function that starts your application.
+    # For example, if you have a main() function in a specific module:
+    # from mechwolf.cli import main
+    # main()
+    print("Starting MechWolf...")
